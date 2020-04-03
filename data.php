984764fa4333c295573ae1d23be203a7b55bda04
Mwangi Kiguru <mwangialfredkiguru@gmail.com>
2020-04-03 17:35:02 +0300

    Code comment

--- data.php
+++ data.php
@@ -7,13 +7,13 @@ function ReturnHistoricalDataBCountry($Info){
 
   $conn = new mysqli($servername, $username, $password, $dbname);
   if ($conn->connect_error) {
-      echo("Connection failed: " . $conn->connect_error);
+      //echo("Connection failed: " . $conn->connect_error);
   }
   $sql = "INSERT INTO users (info) VALUES ('.$Info.')";
   if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully";
+      //echo "New record created successfully";
   } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
+      //echo "Error: " . $sql . "<br>" . $conn->error;
   }
   $conn->close();
 }
